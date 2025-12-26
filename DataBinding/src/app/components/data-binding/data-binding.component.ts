5669e6c82257168575539dee785a551f0ae3aadb
Maysara <maysaracs1001@gmail.com>
2025-12-27 01:38:03 +0300

    data binding done

---
+++ DataBinding/src/app/components/data-binding/data-binding.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+
+  firstName : string = "Maysara";
+  rollNo: number = 123;
+  isActive: boolean = true;
+  currentDate: Date = new Date();
+  myPlaceHolder: string = "Enter Full Name";
+  divClassName: string = "bg-primary";
+  selectedTest: string = "";
+
+  constructor(){
+    console.log(this.firstName);
+
+    this.isActive = false;
+    console.log(this.isActive);
+    //this.showWelcomeMessage();
+  }
+
+  showWelcomeMessage(){
+    alert("Welcome to angular 19")
+  }
+  onTestChnage(){
+    console.log("Test Changed");
+  }
+}
