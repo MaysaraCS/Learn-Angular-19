9c525476c05d11327bb0e4c9b98105c61c4a1390
Maysara <maysaracs1001@gmail.com>
2025-12-27 15:11:53 +0300

    flowstatement

---
+++ FlowStatement/src/app/components/ng-if/ng-if.component.ts
@@ -0,0 +1,24 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-ng-if',
+  imports: [NgIf, FormsModule],
+  templateUrl: './ng-if.component.html',
+  styleUrl: './ng-if.component.css'
+})
+export class NgIfComponent {
+  div1Visibale: boolean = true;
+
+  number1: string = '';
+  number2: string = '';
+
+  hideDiv1(){
+    this.div1Visibale = false;
+  }
+
+  showDiv1(){
+    this.div1Visibale = true;
+  }
+}
