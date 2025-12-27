11b7faee36af5e0ac2a1adb5f853caea76155954
Maysara <maysaracs1001@gmail.com>
2025-12-27 16:13:08 +0300

    flowstatement done

--- FlowStatement/src/app/components/control-statement/control-statement.component.ts
+++ FlowStatement/src/app/components/control-statement/control-statement.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-statement',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-statement.component.html',
   styleUrl: './control-statement.component.css'
 })
 export class ControlStatementComponent {
+  div1vissibale: boolean = true;
 
+  isChecked: boolean =  false;
+
+  dayName: string = '';
+
+  // array of object 
+  employeeArray: any[] = [
+    {
+      empId:121, 
+      name:'AAA',
+      city:'city one',
+      contactNumber:'11111'
+    },
+    {
+      empId:51515, 
+      name:'bbb',
+      city:'city two',
+      contactNumber:'222'
+    },
+    {
+      empId:333, 
+      name:'ccc',
+      city:'city three',
+      contactNumber:'8888'
+    },
+    {
+      empId:999, 
+      name:'lll',
+      city:'city four',
+      contactNumber:'84515'
+    },
+  ]
+  
+  cityList: string[] = [
+    "city1",
+    "city2",
+    "city3",
+    "city4"
+  ];
+
+  hideShowDiv1(isShow: boolean){
+    this.div1vissibale =  isShow;
+  }
 }
