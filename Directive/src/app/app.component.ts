5b90d52c905ac44a68109c1aa96201278ff1a4b2
Maysara <maysaracs1001@gmail.com>
2025-12-27 14:41:06 +0300

    directive done

--- Directive/src/app/app.component.ts
+++ Directive/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgClassComponent } from './components/ng-class/ng-class.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
@@ -5,10 +6,11 @@ import { UserComponent } from './components/user/user.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { NgIfComponent } from './components/ng-if/ng-if.component';
 import { NgForComponent } from './components/ng-for/ng-for.component';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NgForComponent],
+  imports: [NgStyleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
